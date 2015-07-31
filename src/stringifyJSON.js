7eb0423130ba6dd9c3d2971bdae73745caca580f
Cynthia Chen <checy16@gmail.com>
2015-07-30 23:56:25 -0700

    "Create stringifyJSON function"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,39 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+ console.log(typeof obj);
+ console.log(Array.isArray(obj));
+  //base case:
+  if (typeof obj === "number") {
+    return obj.toString();
+  }
+  if (obj === null) {
+     return "null";
+  }
+
+  if (typeof obj === "boolean") {
+      return obj.toString();
+  }
+
+  if (typeof obj === "string") {
+     return "\"" + obj + "\"";
+  }
+    if (Array.isArray(obj)) {
+    var result = [];
+    _.each(obj, function(item) {
+      result.push(stringifyJSON(item));
+    });
+    return "[" + result.join(",") + "]";
+  } else {
+    var str = [];
+    for (var prop in obj) {
+      if (obj[prop] === "undefined" || typeof obj[prop] === "function") {
+        return "{}";
+      } else {
+      str.push(stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]));
+      }
+    }
+    return "{" + str.join(",") + "}";
+  }
+
 };
